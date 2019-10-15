7530f3e7ebe083d1f0da0e0b9fc4f8ae04a6fda1
Amanzhol Umurzakov <umurzakov.amanzhol@gmail.com>
2019-10-15 18:29:34 +0600

    Updates

---
+++ precache-manifest.7e2b8b7c2c5cb2bb6972fecd1fc14092.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eefd680104f8b3d2707465e3c56d6a2",
+    "url": "/umurzakov/index.html"
+  },
+  {
+    "revision": "878e3367d9e53b291476",
+    "url": "/umurzakov/static/css/main.77c923fa.chunk.css"
+  },
+  {
+    "revision": "4dc44154cb5e0686c8dd",
+    "url": "/umurzakov/static/js/2.b1e5fe29.chunk.js"
+  },
+  {
+    "revision": "878e3367d9e53b291476",
+    "url": "/umurzakov/static/js/main.be68c18c.chunk.js"
+  },
+  {
+    "revision": "351362cbb936ea5979a9",
+    "url": "/umurzakov/static/js/runtime-main.8632ca7c.js"
+  },
+  {
+    "revision": "e3af08b302b9e1cd64f91330f46078de",
+    "url": "/umurzakov/static/media/offerImg.e3af08b3.jpg"
+  },
+  {
+    "revision": "58644f1c9d8f0ca01aea8ebce5fbaf7b",
+    "url": "/umurzakov/static/media/worksImg.58644f1c.jpg"
+  }
+]);
